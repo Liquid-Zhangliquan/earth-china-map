abd496dc18308dd238a7c0f9eed9cb1b63143bec
Liquid-Zhangliquan <1063754495@qq.com>
2020-01-07 18:42:15 +0800

    数据钻取

--- src/views/cesium-earth.vue
+++ src/views/cesium-earth.vue
@@ -4,13 +4,10 @@
   </div>
 </template>
 <script>
-
 export default {
   name: 'cesium-earth',
   data() {
-    return {
-
-    };
+    return {};
   },
   mounted() {
     this.init();
@@ -29,37 +26,104 @@ export default {
         scene3DOnly: true, // 每个几何实例仅以3D渲染以节省GPU内存
         baseLayerPicker: false, // 底图切换控件
         animation: false, // 控制场景动画的播放速度控件
-        terrainProvider: new Cesium.createWorldTerrain() // 世界地形
-        // imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-        //   url:
-        //     "https://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=39a4ceb78b2abd7a076551494f1c4fd0",
-        //   layer: "tdtImgBasicLayer",
-        //   style: "default",
-        //   format: "image/jpeg",
-        //   tileMatrixSetID: "GoogleMapsCompatible",
-        //   show: false
-        // })
-        // infoBox : false,
-        // selectionIndicator : false,
-        // shadows : true,
-        // shouldAnimate : true
+        terrainProvider: new Cesium.createWorldTerrain(), // 世界地形
+        selectionIndicator: false, // 追踪entity
+        infoBox: false // 默认弹窗
       };
 
       const viewer = new Cesium.Viewer('cesiumContainer', viewerOption);
-      viewer.scene.globe.enableLighting = true;
+      // viewer.scene.globe.enableLighting = true;
       // viewer.scene.globe.depthTestAgainstTerrain = true; // 防止模型漂移
       viewer._cesiumWidget._creditContainer.style.display = 'none'; // 隐藏版权
-      this.viewer = viewer;
       window.viewer = viewer;
 
       this.initCameraEnd();
       this.initScenePick();
+      this.initClickHandle();
+      this.initMoveHandle();
+      this.loadGeojson('china');
 
       // 飞行至指定位置
       viewer.camera.flyTo({
-        destination: Cesium.Cartesian3.fromDegrees(currentView.lon, currentView.lat, currentView.height),
-        orientation: currentView.orientation
+        destination: Cesium.Cartesian3.fromDegrees(113.42192637406042, 29.96121312975188, 10864227.521069918),
+        orientation: {
+          heading: 6.166585806532539,
+          pitch: -1.547499811469332,
+          roll: 0
+        }
       });
+    },
+    loadGeojson(key) {
+      const url = '../static/data/' + key + '.json';
+      viewer.dataSources.removeAll();
+      // viewer.entities.removeAll();
+      const promise = Cesium.GeoJsonDataSource.load(url);
+      promise.then(dataSource => {
+        viewer.dataSources.add(dataSource);
+        const entities = dataSource.entities.values;
+        const colorHash = {};
+        for (let i = 0; i < entities.length; i++) {
+          const entity = entities[i];
+          const name = entity.name;
+          const color = colorHash[name];
+          if (!color) {
+            color = Cesium.Color.fromRandom({
+              alpha: 0
+            });
+            colorHash[name] = color;
+          }
+          entity.pickType = key;
+          entity.polygon.material = color;
+          entity.polygon.outline = true;
+          (entity.polygon.outlineWidth = 100), (entity.polygon.outlineColor = Cesium.Color.fromBytes(0, 173, 255, 255));
+          entity.polygon.extrudedHeight = 10000.0;
+          // viewer.entities.add(entity)
+        }
+      });
+      key !== 'china' ? viewer.flyTo(promise) : null;
+    },
+    initClickHandle() {
+      const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+      handler.setInputAction(movement => {
+        const pick = viewer.scene.pick(movement.position);
+        if (Cesium.defined(pick) && pick.id.pickType === 'china') {
+          const adcode = pick.id.properties.adcode._value;
+          const name = pick.id.properties.name._value;
+          alert(name);
+          this.loadGeojson(adcode);
+        }
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    },
+    initMoveHandle() {
+      debugger
+      const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+      handler.setInputAction(movement => {
+        const pick = viewer.scene.pick(movement.startPosition);
+        if (Cesium.defined(pick) && pick.id.pickType === 'china') {
+          const entity = pick.id;
+          const name = entity.properties.name._value;
+          if (this.focusEntity) {
+            if (this.focusEntity.id === entity.id) return;
+          }
+          const color = Cesium.Color.fromRandom({
+            alpha: 0
+          });
+          const entities = viewer.dataSources._dataSources[0].entities.values
+          // const entities = viewer.entities.values;
+          entities.map((_entity) => {
+            _entity.polygon.material = color;
+          })
+          const HldColor = Cesium.Color.YELLOW;
+          entity.polygon.material = HldColor;
+          this.focusEntity = entity;
+        } else if(this.focusEntity) {
+          // const color = Cesium.Color.fromRandom({
+          //   alpha: 0
+          // });
+          // this.focusEntity.polygon.material = color;
+          // this.focusEntity = null
+        }
+      }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
     },
     initCameraEnd() {
       this.camera = viewer.camera;
